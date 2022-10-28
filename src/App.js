b2aeb0868f5ec6683caa95fcce751dcaeb3f705a
Viktor Larin <Larinv1983@mail.ru>
2022-10-28 09:50:57 +0300

    sass and block.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Block  from './Block';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-    
+    <Block/>
+    <Block/>
     </div>
   );
 }
